d11b7301867f6b3a336da90ae9b31f58709f00da
Shepharz <espinoza.carlos64@uabc.edu.mx>
2022-05-30 00:49:47 -0700

    Mapa utilizando servicios express

---
+++ configs/dbconfig.js
@@ -0,0 +1,13 @@
+const config ={
+
+    db: {
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database:"tacolizador",
+        port:3306
+
+    }
+};
+
+module.exports=config;
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+const routerTaqueria=require('./routes/taqueria.router');
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
+});
+app.use('/api/v1/taquerias',routerTaqueria);
+app.listen(port, () => {
+  console.log('Mi port:' +  port);
+});
+
